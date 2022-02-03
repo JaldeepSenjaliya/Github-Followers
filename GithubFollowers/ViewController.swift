b40d94c38d5ee2d954fff1fd5df60c0ad1cf1a1a
Jaldeep <jaldeepsenjaliya@gmail.com>
2022-02-03 10:19:11 -0500

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Jaldeep Patel on 2022-02-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
