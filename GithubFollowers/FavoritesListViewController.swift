079786500bf9fef7c96ec0da65cfe3ccfbd4e6b2
Jaldeep <jaldeepsenjaliya@gmail.com>
2022-02-03 12:56:04 -0500

    Setup Tabbar Controller, Navigation Controller and Customized Button and TextField

--- GithubFollowers/FavoritesListViewController.swift
+++ GithubFollowers/FavoritesListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListViewController.swift
 //  GithubFollowers
 //
 //  Created by Jaldeep Patel on 2022-02-03.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
+    
 }
-
---
+++ GithubFollowers/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GithubFollowers
+//
+//  Created by Jaldeep Patel on 2022-02-03.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+}
